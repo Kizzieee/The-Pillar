1cfbe19c2050f4e73931aa8492f58e7b17e300c2
Kizzelyn <kizseaf@gmail.com>
2023-04-03 10:17:24 +0800

    Okay na an about, contact, and userprofile

---
+++ script/custom-script.js
@@ -0,0 +1,46 @@
+
+      function addScrollingNavbarBehavior(navbarClass) {
+        const navbarElements = document.querySelectorAll(navbarClass);
+        navbarElements.forEach(function(navlink) {
+          window.addEventListener('scroll', function() {
+            if (window.scrollY > 100) {
+              navlink.classList.add('scrolled');
+            } else {
+              navlink.classList.remove('scrolled');
+            }
+          });
+        });
+      }
+      addScrollingNavbarBehavior('.navLinks');
+
+
+
+      function addAboutScrollingNavbarBehavior(navbarId, brandNameId, iconClass) {
+        const navbar = document.getElementById(navbarId);
+        const brandName = document.getElementById(brandNameId);
+        const accountIcon = document.getElementById(iconClass);
+        window.addEventListener('scroll', function() {
+          if (window.scrollY > 100) {
+            navbar.classList.add('scrolled');
+
+            brandName.classList.remove('d-none');
+            brandName.classList.add('d-block');
+
+            accountIcon.classList.remove('text-primary');
+            accountIcon.classList.add('text-white');
+          } else {
+            navbar.classList.remove('scrolled');
+
+            brandName.classList.remove('d-block');
+            brandName.classList.add('d-none');
+
+          
+            accountIcon.classList.remove('text-white');
+            accountIcon.classList.add('text-primary');
+          }
+        });
+      }
+      addAboutScrollingNavbarBehavior('mainNavAbout', 'brandName', 'accountIcon');
+      
+
+      
